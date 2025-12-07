28e4e06308c124e5e8c5f98c079a93d277b0245f
robin.bygrave <robin.bygrave@eroad.com>
2025-12-08 12:45:06 +1300

    [oauth2-core] Fix http client dependency (from avaje to plain java client)
    - avaje-oauth2-core only needs the plain Java HttpClient
    - avaje-oauth2-helidon-jwtfilter compile to 21 (rather than 23?)

--- avaje-oauth2-core/src/main/java/module-info.java
+++ avaje-oauth2-core/src/main/java/module-info.java
@@ -4,5 +4,5 @@ module io.avaje.oauth2.core {
     exports io.avaje.oauth2.core.jwt;
 
     requires transitive io.avaje.json;
-    requires transitive io.avaje.http.client;
+    requires transitive java.net.http;
 }
